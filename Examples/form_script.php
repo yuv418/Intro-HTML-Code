139c7fe4121586ca011c0098235794c6dde3b314
cdknight <enterprisebalaji@hotmail.com>
2017-02-06 22:55:34 -0500

    hi

---
+++ Examples/form_script.php
@@ -0,0 +1,10 @@
+
+<?php
+$name = $_POST['name'];
+$EMail = $_POST['Email'];
+$country = $_POST['Country'];
+$age = $_POST['age'];
+echo "<p>Name: $name </br> EMail: $EMail </br> Country: $country </br> Age: $age</p>";
+?>
+
+

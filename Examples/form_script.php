72d6da699fcaa1351839f008964be76f7c405075
cdknight <enterprisebalaji@hotmail.com>
2017-02-08 15:26:12 -0500

    hi

---
+++ Examples/form_script.php
@@ -0,0 +1,35 @@
+<!DOCTYPE html>
+<html>
+<head>
+	 <link rel="stylesheet" href="../styles.css">
+</head>
+<body>
+	
+<ul>
+<li><a href="../index.html">Home</a></li>
+<li><a href="../howto.html">How To</a></li>
+<li><a href ="../examples.html">Examples</a></li>
+
+
+
+
+</ul>
+<p></p>
+<?php
+$name = $_POST['name'];
+$EMail = $_POST['Email'];
+$country = $_POST['Country'];
+$age = $_POST['age'];
+print "<p>Name: $name </br> EMail: $EMail </br> Country: $country </br> Age: $age</p>";
+
+?>
+<p></p>
+<a href="php_example.html"><button class="coolbutton">Back</button></a>
+
+
+</body>
+
+
+</html>
+
+
